5f00072336a08c46bfd1de18d9c48a28dc536651
admin <admin@sxt.com>
2020-12-22 09:16:20 +0800

    第一次提交

---
+++ ttt.java
@@ -0,0 +1,28 @@
+/**
+ * @Auther:lisuibing
+ * @Date: 2020/12/22
+ * @Description: PACKAGE_NAME
+ * @Version: 1.0
+ */
+public class ttt {
+    private static String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+     public static void main(String[] args){
+             System.out.println(username);
+     }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
