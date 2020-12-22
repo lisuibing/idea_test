3ea54d2e3c46dbf0fa57f5cfacf1182d93ac309a
admin <admin@sxt.com>
2020-12-22 10:06:32 +0800

    第二次提交

--- ttt.java
+++ ttt.java
@@ -7,6 +7,7 @@
 public class ttt {
     private static String username;
     private String password;
+    private String passwor;
 
     public String getUsername() {
         return username;
@@ -24,5 +25,6 @@ public class ttt {
 
     public void setPassword(String password) {
         this.password = password;
+
     }
 }
